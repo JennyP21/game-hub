c3c57ac8fc002c586b1a6a699b05868783056207
Jenny Patel <pateljenny555@gmail.com>
2023-03-21 20:41:00 -0700

    Return null on error GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,19 +1,14 @@
-import {
-  HStack,
-  Image,
-  List,
-  ListItem,
-  Skeleton,
-  Spinner,
-  Text,
-} from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { data, isLoading } = useGenres();
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   return (
     <>
       {isLoading ? (
