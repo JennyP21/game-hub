99b9189bbbcf33ded1182b470ff69913c66ac9dc
Jenny Patel <pateljenny555@gmail.com>
2023-08-01 21:14:27 -0700

    Refactor - Simplify game Query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,10 +5,10 @@ import GenreListSkeleton from "./GenreListSkeleton";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -31,7 +31,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
                 whiteSpace='normal'
                 textAlign='left'
                 fontWeight={
-                  genre.id === selectedGenre?.id ? "bold" : "normal"
+                  genre.id === selectedGenreId ? "bold" : "normal"
                 }
                 onClick={() => onSelectGenre(genre)}
                 fontSize="lg"
